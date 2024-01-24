28c5dd90a55a0a3ab1749723a34a8e6813a6efef
Tilman Vatteroth <git@tilmanvatteroth.de>
2024-01-24 09:02:17 +0100

    add dotenv and improve dates

--- index.mjs
+++ index.mjs
@@ -1,4 +1,8 @@
 import {InfluxDB, Point} from "@influxdata/influxdb-client";
+import {config} from "dotenv";
+import {DateTime} from "luxon";
+
+config()
 
 const token = process.env.INFLUXDB_TOKEN
 const url = process.env.INFLUXDB_URL
@@ -8,20 +12,22 @@ const client = new InfluxDB({url, token})
 
 async function findLastDate(client, fieldName) {
     const res = await client.getQueryApi(org).collectRows(`from(bucket: "awattar")
-  |> range(start: 0h, stop: 100000h)
+  |> range(start: -100000h, stop: 100000h)
   |> filter(fn: (r) => r["_measurement"] == "${fieldName}")
-  |> last()`)
+  |> last()
+  |> sort(columns: ["_time"])`)
 
     if (res.length > 0) {
-        return new Date(new Date(res[0]._time).getTime() + 1)
+        return DateTime.fromISO(res[0]._time).plus(1)
     }
-    return new Date(new Date().getTime() - 24*60*60*1000)
+    return DateTime.now().minus({day: 1})
 }
 
-async function doAwattarApiRequest(endpoint, lastDate) {
-    console.log("Fetching data after", lastDate.toISOString())
+async function doAwattarApiRequest(endpoint, start, end) {
+    console.log("Fetching data from", start.toString(), "to", end.toString())
     const url = new URL(`https://api.awattar.de/v1/${endpoint}`)
-    url.searchParams.set("start", lastDate.getTime())
+    url.searchParams.set("start", start.toMillis())
+    url.searchParams.set("end", end.toMillis())
     const response = await fetch(url)
     return await response.json()
 }
@@ -62,15 +68,17 @@ function processDataPoints(datapoints, writeClient, pointFactory) {
 
 
 const writeClient = client.getWriteApi(org, bucket, 'ns');
+const endOfToday = DateTime.now().endOf("day");
+const endOfTomorrow =  DateTime.now().plus({day: 1}).endOf("day");
 
 const lastMarketPriceDate = await findLastDate(client, 'marketprice');
-const marketdata = await doAwattarApiRequest('marketdata', lastMarketPriceDate)
+const marketdata = await doAwattarApiRequest('marketdata', lastMarketPriceDate, endOfTomorrow)
 console.log("found", marketdata.data.length, "new marketdata entries")
 processDataPoints(marketdata.data, writeClient, convertMarketPriceToPoint)
 
 
 const lastProductionDate = await findLastDate(client, 'production');
-const production = await doAwattarApiRequest('power/productions', lastProductionDate)
+const production = await doAwattarApiRequest('power/productions', lastProductionDate, endOfToday)
 console.log("found", production.data.length, "new production entries")
 processDataPoints(production.data, writeClient, convertProductionToPoint)
 
